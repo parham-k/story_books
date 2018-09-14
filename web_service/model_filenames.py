60fc2d8ada41bf85c413c96f88dcafb8d0e310b5
Parham Kazemi <pkazemi3@gmail.com>
2018-09-14 20:01:19 +0430

    Fixed book contents conversion to zip

---
+++ web_service/model_filenames.py
@@ -0,0 +1,19 @@
+import os
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
+
+
+def get_book_text_filename(instance, filename):
+    return os.path.join('upload', 'books', instance.title, 'text.txt')
+
+
+def get_book_cover_filename(instance, filename):
+    return os.path.join('upload', 'books', instance.title, 'cover.png')
--- web_service/models.py
+++ web_service/models.py
@@ -1,7 +1,11 @@
-from os.path import join
+import os
+import zipfile
+
 from django.db import models
+from django.dispatch import receiver
 
 from story_books_server.settings import BASE_DIR
+from web_service import model_filenames
 
 
 class Book(models.Model):
@@ -9,8 +13,31 @@ class Book(models.Model):
         max_length=255,
     )
     text = models.FileField(
-        upload_to=lambda instance, filename: join(BASE_DIR, 'upload', 'books', instance.title, 'text.txt')
+        upload_to=model_filenames.get_book_text_filename,
+        storage=model_filenames.OverwriteStorage(),
     )
     cover = models.ImageField(
-        upload_to=lambda instance, filename: join(BASE_DIR, 'upload', 'books', instance.title, 'cover.png')
+        upload_to=model_filenames.get_book_cover_filename,
+        storage=model_filenames.OverwriteStorage(),
     )
+
+    def __str__(self):
+        return self.title
+
+    def get_dir_path(self):
+        return os.path.join(BASE_DIR, 'upload', 'books', self.title)
+
+    def get_zip_filename(self):
+        return '{}.zip'.format(os.path.join(self.get_dir_path(), self.title))
+
+
+@receiver(models.signals.post_save, sender=Book)
+def create_zip_file(sender, instance, created, **kwargs):
+    if os.path.exists(instance.get_zip_filename()):
+        os.remove(instance.get_zip_filename())
+    z = zipfile.ZipFile(instance.get_zip_filename(), 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(instance.get_dir_path()):
+        for file in files:
+            if os.path.join(root, file) != instance.get_zip_filename():
+                z.write(os.path.join(root, file), file)
+    z.close()
