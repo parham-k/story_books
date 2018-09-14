1220cd39787a8f0e2549721502acc8c960d737c8
Parham Kazemi <pkazemi3@gmail.com>
2018-09-14 18:01:15 +0430

    Initial commit

---
+++ web_service/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=255,
+    )
+    text = models.FileField()
+    cover = models.ImageField()
