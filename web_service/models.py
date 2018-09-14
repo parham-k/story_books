d57733e5399a17ece0a08fda8259a0f1a3c225bd
Parham Kazemi <pkazemi3@gmail.com>
2018-09-14 18:17:18 +0430

    Fixed book content upload paths

--- web_service/models.py
+++ web_service/models.py
@@ -1,9 +1,16 @@
+from os.path import join
 from django.db import models
+
+from story_books_server.settings import BASE_DIR
 
 
 class Book(models.Model):
     title = models.CharField(
         max_length=255,
     )
-    text = models.FileField()
-    cover = models.ImageField()
+    text = models.FileField(
+        upload_to=lambda instance, filename: join(BASE_DIR, 'upload', 'books', instance.title, 'text.txt')
+    )
+    cover = models.ImageField(
+        upload_to=lambda instance, filename: join(BASE_DIR, 'upload', 'books', instance.title, 'cover.png')
+    )
