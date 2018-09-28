0c72384157dc220f3a7e2397fd12945e6a90af1f
Parham Kazemi <pkazemi3@gmail.com>
2018-09-28 17:20:29 +0330

    Refactored routers

--- web_service/routers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import routers
-
-from web_service import views
-
-router = routers.DefaultRouter()
-router.register('all_books', views.AllBooksViewSet)
--- web_service/urls.py
+++ web_service/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
-from web_service.routers import router
+from rest_framework import routers
+
+from web_service import views
+
+router = routers.DefaultRouter()
+router.register(r'all_books', views.AllBooksViewSet, base_name='Book')
 
 urlpatterns = [
     path('', include(router.urls)),
