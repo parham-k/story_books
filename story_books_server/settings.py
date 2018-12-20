b896369f0e1354746ce656a4c3112a2cdfa88564
Parham Kazemi <pkazemi3@gmail.com>
2018-12-20 15:19:25 +0330

    Updated routing for broadcast

--- story_books_server/settings.py
+++ story_books_server/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'a%zbk+3g)=*c1uxcf=_0drfw&th2msgua0b)t7%3-gqds!_05x'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
