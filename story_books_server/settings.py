5294ca0594b6a372f5478405833cd0065f0a7ace
Parham Kazemi <pkazemi3@gmail.com>
2018-12-20 17:59:23 +0330

    Added media serving urls

--- story_books_server/settings.py
+++ story_books_server/settings.py
@@ -88,6 +88,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
