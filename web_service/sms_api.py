925e7e2ad334f49275634eff7bb3151bd043c410
Parham Kazemi <pkazemi3@gmail.com>
2018-12-28 16:39:45 +0330

    Added sms api

--- web_service/sms_api.py
+++ web_service/sms_api.py
@@ -1,2 +1,22 @@
+import requests
+
+from story_books_server.settings import SMS_API_USERNAME, SMS_API_PASSWORD
+
+
 def send_sms(phone, text):
-    pass
+    sms_url = 'http://37.130.202.188/api/select'
+    data = {
+        "op": "send",
+        "uname": SMS_API_USERNAME,
+        "pass": SMS_API_PASSWORD,
+        "message": text,
+        "from": "",
+        "to": [phone],
+    }
+    response = requests.post(sms_url, data)
+    print(response.content)
+    return response.json()
+
+
+if __name__ == '__main__':
+    print(send_sms('09134231834', 'تست پیامک'))
